4916761669beb543422a5239d7086645bb27898a
Habib-qazwsx <roaringhabib64@gmail.com>
2022-08-24 12:16:59 +0600

    add downloadTask

---
+++ src/downloadTask/DownloadTask.java
@@ -0,0 +1,59 @@
+package downloadTask;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.concurrent.CountDownLatch;
+
+public class DownloadTask implements Runnable{
+    private URL downloadUrl;
+    private long startByte;
+    private long partSize;
+    private String partName;
+    private String dowloadDir;
+    private CountDownLatch countDownLatch;
+
+    public DownloadTask(URL downloadUrl, long startByte, long partSize, String partName, String dowloadDir, CountDownLatch countDownLatch) {
+        this.downloadUrl = downloadUrl;
+        this.startByte = startByte;
+        this.partSize = partSize;
+        this.partName = partName;
+        this.dowloadDir = dowloadDir;
+        this.countDownLatch = countDownLatch;
+    }
+
+    @Override
+    public void run(){
+        try {
+            final HttpURLConnection connection = getConnection();
+
+            try (ReadableByteChannel rbc = Channels.newChannel(connection.getInputStream())){
+                FileOutputStream outputStream = new FileOutputStream(getPartFileName());
+                outputStream.getChannel().transferFrom(rbc, 0, connection.getContentLength());
+
+                countDownLatch.countDown();
+            }
+        } catch (IOException e){
+            System.err.println("Failed to download the file: " + e.getMessage());
+            Thread.currentThread().interrupt();
+            throw new AssertionError(e);
+        }
+    }
+
+    private String getPartFileName () {
+        return dowloadDir + File.separator + partName;
+    }
+
+    private HttpURLConnection getConnection() throws IOException{
+        HttpURLConnection connection = (HttpURLConnection) downloadUrl.openConnection();
+        String downloadRange = "bytes=" + startByte + "-" + (startByte + partSize);
+
+        connection.setRequestProperty("Range", downloadRange);
+        connection.connect();
+        return connection;
+    }
+}
